0b9ff606201faef3c124b7c35f32f3ac55d89760
Maria Shevchenko <shevtma@yandex.ru>
2023-03-06 01:04:53 +0300

    updated params for the local runs

---
+++ .profile
@@ -0,0 +1,5 @@
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH=$JAVA_HOME/bin:$PATH
+export ANDROID_HOME=/Users/maria/Library/Android/sdk
+export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
--- config/wdio.android.browser.conf.ts
+++ config/wdio.android.browser.conf.ts
@@ -21,8 +21,8 @@ config.capabilities = [
         // For W3C the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_3_10.0',
-        'appium:platformVersion': '10.0',
+        'appium:deviceName': 'Pixel_6_13.0',
+        'appium:platformVersion': '13.0',
         'appium:orientation': 'PORTRAIT',
         'appium:newCommandTimeout': 240,
     },
